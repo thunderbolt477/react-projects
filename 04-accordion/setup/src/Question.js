bfdd4aba036d8f49460c21fb14320a0e0be0b874
Timothy Balilo <ts.balilo@gmail.com>
2022-06-23 15:46:52 -0700

    project 4/15

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return(
+    <article className='question'>
+      <header>
+        <h2>{title}</h2>
+        <button className='btn>' onClick={() => setShowInfo(!showInfo)}>{showInfo? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+      </header>
+      {showInfo  && <p>{info}</p>}
+    </article>
+  )
 };
 
 export default Question;
